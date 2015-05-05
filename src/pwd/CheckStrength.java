2de36a8ef232bebd34d0faf11e612bdf00d39c52
bertoche <uriel.bertoche@uniriotec.br>
2015-05-05 05:17:25 -0300

    adicionada regra para caixa de letra, adicionado teste para numericos

--- src/pwd/CheckStrength.java
+++ src/pwd/CheckStrength.java
@@ -13,13 +13,8 @@ package pwd;
 public class CheckStrength {
 
 	public enum LEVEL {
-		EASY, MIDIUM, STRONG, VERY_STRONG, EXTREMELY_STRONG, WRONG_PASSWORD
-		}
-
-	private static final int NUM = 1;
-	private static final int SMALL_LETTER = 2;
-	private static final int CAPITAL_LETTER = 3;
-	private static final int OTHER_CHAR = 4;
+            EASY, MIDIUM, STRONG, VERY_STRONG, EXTREMELY_STRONG, WRONG_PASSWORD
+        }
 	
 	private static //Parametros de teste
 	String[] partialRegexChecks = { ".*[a-z]+.*", // lowercase
@@ -29,51 +24,6 @@ public class CheckStrength {
 	};
 	
 	private static final String PASSWORD_PATTERN_FULL = "((?=.*[a-z])(?=.*\\d)(?=.*[A-Z])(?=.*[@#$%!]).{8,40})";
-
-
-	/**
-	 * Simple password dictionary
-	 */
-	private final static String[] DICTIONARY = { "password", "abc123", "iloveyou", "adobe123", "123123", "sunshine",
-			"1314520", "a1b2c3", "123qwe", "aaa111", "qweasd", "admin", "passwd" };
-
-	/**
-	 * Check character's type, includes num, capital letter, small letter and other character.
-	 * 
-	 * @param c
-	 * @return
-	 */
-	private static int checkCharacterType(char c) {
-		if (c >= 48 && c <= 57) {
-			return NUM;
-		}
-		if (c >= 65 && c <= 90) {
-			return CAPITAL_LETTER;
-		}
-		if (c >= 97 && c <= 122) {
-			return SMALL_LETTER;
-		}
-		return OTHER_CHAR;
-	}
-
-	/**
-	 * Count password's number by different type
-	 * 
-	 * @param passwd
-	 * @param type
-	 * @return
-	 */
-	private static int countLetter(String passwd, int type) {
-		int count = 0;
-		if (null != passwd && passwd.length() > 0) {
-			for (char c : passwd.toCharArray()) {
-				if (checkCharacterType(c) == type) {
-					count++;
-				}
-			}
-		}
-		return count;
-	}
 
 	/**
 	 * Check password's strength
@@ -108,71 +58,13 @@ public class CheckStrength {
 		}
 	}
 
-	private static int pwdLowLevel(String passwd, int len, int level) {
-		if ("abcdefghijklmnopqrstuvwxyz".indexOf(passwd) > 0 || "ABCDEFGHIJKLMNOPQRSTUVWXYZ".indexOf(passwd) > 0) {
-			level--;
-		}
-		if ("qwertyuiop".indexOf(passwd) > 0 || "asdfghjkl".indexOf(passwd) > 0 || "zxcvbnm".indexOf(passwd) > 0) {
-			level--;
-		}
-
-		if (countLetter(passwd, SMALL_LETTER) == len
-				|| countLetter(passwd, CAPITAL_LETTER) == len) {
-			level--;
-		}
-
-		if (len % 2 == 0) { // aaabbb
-			String part1 = passwd.substring(0, len / 2);
-			String part2 = passwd.substring(len / 2);
-			if (part1.equals(part2)) {
-				level--;
-			}
-			if (StringUtils.isCharEqual(part1) && StringUtils.isCharEqual(part2)) {
-				level--;
-			}
-		}
-		if (len % 3 == 0) { // ababab
-			String part1 = passwd.substring(0, len / 3);
-			String part2 = passwd.substring(len / 3, len / 3 * 2);
-			String part3 = passwd.substring(len / 3 * 2);
-			if (part1.equals(part2) && part2.equals(part3)) {
-				level--;
-			}
-		}
-
-		return level;
-	}
-
 	private static int pwdUpLevel(String passwd, int len) {
 		int level = 0;
 	
-			//uso de expressões regular para checar o tamanho e se atende a os 4 padrões
-			if (passwd.matches(PASSWORD_PATTERN_FULL)) {
-				level++;
-			}
-		
-			if (len > 6 && countLetter(passwd, NUM) >= 3 && countLetter(passwd, SMALL_LETTER) >= 3
-					|| countLetter(passwd, NUM) >= 3 && countLetter(passwd, CAPITAL_LETTER) >= 3
-					|| countLetter(passwd, NUM) >= 3 && countLetter(passwd, OTHER_CHAR) >= 2
-					|| countLetter(passwd, SMALL_LETTER) >= 3 && countLetter(passwd, CAPITAL_LETTER) >= 3
-					|| countLetter(passwd, SMALL_LETTER) >= 3 && countLetter(passwd, OTHER_CHAR) >= 2
-					|| countLetter(passwd, CAPITAL_LETTER) >= 3 && countLetter(passwd, OTHER_CHAR) >= 2) {
-				level++;
-			}
-		
-			if (len > 8 && countLetter(passwd, NUM) >= 2 && countLetter(passwd, SMALL_LETTER) >= 2
-					&& countLetter(passwd, CAPITAL_LETTER) >= 2 || countLetter(passwd, NUM) >= 2
-					&& countLetter(passwd, SMALL_LETTER) >= 2 && countLetter(passwd, OTHER_CHAR) >= 2
-					|| countLetter(passwd, NUM) >= 2 && countLetter(passwd, CAPITAL_LETTER) >= 2
-					&& countLetter(passwd, OTHER_CHAR) >= 2 || countLetter(passwd, SMALL_LETTER) >= 2
-					&& countLetter(passwd, CAPITAL_LETTER) >= 2 && countLetter(passwd, OTHER_CHAR) >= 2) {
-				level++;
-			}
-		
-			if (len > 10 && countLetter(passwd, NUM) >= 2 && countLetter(passwd, SMALL_LETTER) >= 2
-					&& countLetter(passwd, CAPITAL_LETTER) >= 2 && countLetter(passwd, OTHER_CHAR) >= 2) {
-				level++;
-			}
+                //uso de expressões regular para checar o tamanho e se atende a os 4 padrões
+                if (passwd.matches(PASSWORD_PATTERN_FULL)) {
+                        level++;
+                }
 		
 			if (countLetter(passwd, OTHER_CHAR) >= 3) {
 				level++;
